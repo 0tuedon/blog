9d968dd02dbf9fefc61fc09be8825f8bb6228c92
Tueddy-twets <tuedontuoyo@gmail.com>
2021-09-23 16:10:47 +0100

    modified ejs files and index.js to add postSchema and use the find({})

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const   path                        =   require('path'),
         expressEdge                 =   require('express-edge'),
         express                     =   require('express'),
         mongoose                    =   require('mongoose'),
+        bodyParser                  =   require('body-parser'),
+        Post                        =   require('./models/post');
         app                         =   express(),
 
         port                        =   process.env.PORT || 4000
@@ -13,8 +15,15 @@ mongoose.connect(severMongoose, {useNewUrlParser:true})
     .catch(err=> console.error('There was an Error SomeWhere' , err))
 app.use(express.static('public'))
 app.set('view engine','ejs');
-app.get('/', (req,res)=>{
-    res.render(path.resolve(__dirname, 'views/index.ejs'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.get('/', async (req,res)=>{
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 });
 
 app.get('/about', (req,res)=>{
@@ -26,10 +35,31 @@ app.get('/contact',(req,res)=>{
 })
 
 app.get('/post',(req,res)=>{
-    res.render('post');
+    res.render('index.js');
 })
 app.get('/post/new',(req,res)=>{
     res.render('create')
+})
+app.post('/post/store', async (req,res)=>{
+    console.log(req.body)
+    await Post.create(req.body, 
+         (error,post) =>{
+             if(err){
+                 console.log(err);
+             }
+             else{
+            res.redirect('/');
+            console.log(post);
+             }
+        })
+})
+
+app.get('/post/:id', async (req,res)=>{
+    var id = req.params.id
+    const post = await Post.findById(id)
+    res.render('post',{
+        post
+    });
 })
 app.listen(port,()=>{
     console.log('App Listening on port ' + port);
